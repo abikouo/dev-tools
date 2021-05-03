91f58450cb664bc8de3d1d8afd345c0d98de6f38
aubin <abikouo@redhat.com>
2021-05-03 10:57:57 +0200

    ansible developer

--- k8s/build_kubeconfig.sh
+++ k8s/build_kubeconfig.sh
@@ -3,13 +3,17 @@
 set -eux
 
 file=$1
+kube_config="~/.kube/config"
+
+if [ "$#" -ne 1 ]; then
+	kube_config=$2
+fi
 
 server=$(cat $file | awk '{ print $2 }' | grep "K8S_AUTH_HOST" | cut -d '=' -f2)
 apikey=$(cat $file | awk '{ print $2 }' | grep "K8S_AUTH_API_KEY" | cut -d '=' -f2)
 
-
-kubectl config set-cluster dev_0 --server="${server}" --insecure-skip-tls-verify
-kubectl config set-credentials user_0 --token="${apikey}"
-kubectl config set-context ansible --cluster=dev_0 --user=user_0
-kubectl config view
-kubectl config use-context ansible
+kubectl config set-cluster dev_0 --server="${server}" --insecure-skip-tls-verify --kubeconfig ${kube_config}
+kubectl config set-credentials user_0 --token="${apikey}" --kubeconfig ${kube_config}
+kubectl config set-context ansible --cluster=dev_0 --user=user_0  --kubeconfig ${kube_config}
+kubectl config view --kubeconfig ${kube_config}
+kubectl config use-context ansible --kubeconfig ${kube_config}
